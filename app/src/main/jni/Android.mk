065522327d9d12385d973b2a6ce10aa28786a73f
Ryan Welton <ryan.g.welton@gmail.com>
2015-08-25 14:59:38 -0400

    Fix all the build things

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -53,8 +53,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := CVE-2014-4943
 LOCAL_SRC_FILES := l2tp_exploit_check.c
-
-LOCAL_CFLAGS += -Ijni/include/
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/
 
 include $(BUILD_SHARED_LIBRARY)
 ################################
@@ -64,8 +63,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := CVE-2014-4943check
 LOCAL_SRC_FILES := l2tp_exploit_check.c
-
-LOCAL_CFLAGS += -Ijni/include/
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/
 
 include $(BUILD_EXECUTABLE)
 ###############################
@@ -76,6 +74,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := cve-2014-3153
 LOCAL_SRC_FILES := futex_exploit_check.c
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/
 
 LOCAL_CFLAGS += -Ijni/include/
 
@@ -87,8 +86,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := cve-2014-3153check
 LOCAL_SRC_FILES := futex_exploit_check.c
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/
 
-LOCAL_CFLAGS += -Ijni/include/
 
 include $(BUILD_EXECUTABLE)
 ################################
@@ -150,7 +149,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := pingpong
 LOCAL_SRC_FILES := ping_pong.c
-LOCAL_CFLAGS    := -fpie -pie -Ijni/include
+LOCAL_CFLAGS    := -fpie -pie
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/
 LOCAL_LDFLAGS   := -pie
 
 include $(BUILD_SHARED_LIBRARY)
@@ -162,7 +162,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := pingpongCheck
 LOCAL_SRC_FILES := ping_pong.c
-LOCAL_CFLAGS    := -fpie -pie -Ijni/include
+LOCAL_CFLAGS    := -fpie -pie
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/
 
 include $(BUILD_EXECUTABLE)
 ################################
@@ -173,8 +174,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := pingpongCheck-pie
 LOCAL_SRC_FILES := ping_pong.c
-LOCAL_CFLAGS    := -fpie -pie -Ijni/include
+LOCAL_CFLAGS    := -fpie -pie
 LOCAL_LDFLAGS   := -pie
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/
 
 include $(BUILD_EXECUTABLE)
 ################################
