73631628f1f95b374f89467cb1cc91cde4dd66f8
Ryan Welton <ryan.g.welton@gmail.com>
2015-09-01 22:52:15 -0400

    Whitespace cleanup

--- app/src/main/jni/graphics_into_overflow_test.c
+++ app/src/main/jni/graphics_into_overflow_test.c
@@ -21,29 +21,29 @@ int main( int argc, char *argv[] )
     int r3[ 10 ];
     int jellybean = 0;
     int ( *unflatten )( int *r0, int *r1, int *r2, int *r3 ) = NULL;
-    
+
     printf( "hello world\n" );
     fflush( stdout );
-    
+
     void *handle = dlopen( libname, RTLD_NOW | RTLD_GLOBAL );
     if( !handle )
     {
         LOG_D( "error opening %s: %s\n", libname, dlerror() );
         return -1;
     }
-    
+
     bzero( classBuf, sizeof( classBuf ) );
     bzero( r1, sizeof( r1 ) );
     bzero( r2, sizeof( r2 ) );
     bzero( r3, sizeof( r3 ) );
-    
+
     int ( *constructor )( int *r0 ) = dlsym( handle, "_ZN7android13GraphicBufferC2Ev" );
     if( !constructor )
     {
         LOG_D( "missing android::GraphicBuffer::GraphicBuffer(void)\n" );
         return -1;
     }
-    
+
     unflatten = dlsym( handle, "_ZN7android13GraphicBuffer9unflattenERPKvRjRPKiS4_" );
     if( !unflatten )
     {
@@ -55,28 +55,28 @@ int main( int argc, char *argv[] )
         }
         jellybean = 1;
     }
-    
+
     constructor( classBuf );
-    
-    
+
+
     // setup bad values
     int r1Ref = (int)(&r1[0]);
-    
+
     // this must match
     r1[0] = 0x47424652;
-    
+
     // size must be > 0x1f
     r2[0] = 0x20;
-    
+
     // attempt to overflow
     r1[8] = 0x1000;
     r1[9] = 0xFF5;
-    
+
     // make sure we error out on unpatched libs before getting to the point where we corrupt the heap
     r1[6] = 0x20;
     r1[7] = 0x20;
-    
-    
+
+
     int ret = 0;
     if( !jellybean )
     {
@@ -87,9 +87,9 @@ int main( int argc, char *argv[] )
         int * val = (int*)(r2[0]);
         ret = unflatten( classBuf, r1, val, r3 );
     }
-    
-    
-    
+
+
+
     // -12 = unpatched 4.4.2
     // -22 = patches 5.1.1
     switch( ret )
@@ -105,8 +105,8 @@ int main( int argc, char *argv[] )
         break;
     }
 
-    
-    
+
+
     return 0;
-    
+
 }
