9616e36fc97fa866df36f90511c445cb3032844e
Patrick McLaughlin <me@patmclaughl.in>
2015-10-11 15:20:54 -0400

    Add crash check wrapper executable

---
+++ app/src/main/jni/crash_check.c
@@ -0,0 +1,71 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+
+static int BAD_COMMAND_STATUS = 42;
+static pid_t childPid = 0;
+
+void quit(int code) {
+  if (childPid > 0) {
+    kill(childPid, SIGKILL);
+    exit(code);
+  }
+}
+
+void child_handler(int sig) {
+  int   status;
+  pid_t pid = waitpid(childPid, &status, WNOHANG);
+  if (pid == -1) {
+    printf("Child didn't exit wat\n");
+    quit(3);
+  }
+
+  if (WIFEXITED(status)) {
+    if (WEXITSTATUS(status) == BAD_COMMAND_STATUS) {
+      printf("Error executing child process\n");
+      quit(4);
+    } else {
+      printf("Child exited normally\n");
+      quit(0);
+    }
+  } else {
+    printf("Boom goes the dynamite\n");
+    quit(1);
+  }
+}
+
+int main(int argc, char** argv, char** envp) {
+  if (argc < 3) {
+    printf("Usage: crashCheck wait_seconds program [args]\n");
+    exit(-1);
+  }
+
+  int numSeconds = atoi(argv[1]);
+  if (numSeconds == 0) {
+    printf("Bad timeout value\n");
+    exit(-1);
+  }
+
+  pid_t pid = fork();
+  if (pid == 0) {
+    execve(argv[2], &argv[2], envp);
+    return BAD_COMMAND_STATUS;
+  } else {
+    childPid = pid;
+
+    struct sigaction sa;
+    bzero(&sa, sizeof(sa));
+    sa.sa_handler = child_handler;
+    sigaction(SIGCHLD, &sa, NULL);
+
+    sleep(numSeconds);
+
+    printf("Child still running\n");
+    quit(2);
+    return 0;
+  }
+}
