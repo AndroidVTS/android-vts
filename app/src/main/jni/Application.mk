b44e5e01243e808ece6a4b518dacc1d359b68b74
S-trace <S-trace@list.ru>
2018-01-28 20:59:38 +0300

    [ARCH] Add arm64-v8a to APP_ABI list
    This is necessary for ARM64 devices

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-APP_ABI := armeabi armeabi-v7a x86
+APP_ABI := armeabi armeabi-v7a x86 arm64-v8a
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include-all
