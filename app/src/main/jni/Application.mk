b8e781aab4fdc97ba15dbc18bba1c59aba27ef4e
Pratik Raj <rajpratik71@gmail.com>
2019-08-01 01:41:16 +0530

    build fixs and upstreamed chanes
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-APP_ABI := armeabi armeabi-v7a x86
+APP_ABI := armeabi-v7a x86
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include-all
